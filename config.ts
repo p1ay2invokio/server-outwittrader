ec6f7c327ea37913c80c751a5e172af28eac1348
play21947 <play21947@gmail.com>
2025-02-27 22:45:31 +0700

    second-commit-1.1

--- config.ts
+++ config.ts
@@ -1,5 +1,3 @@
-type stateType = "dev" | "prod"
-
-export const state: stateType = "dev"
+export let state = 'prod'
 export const PORT = 3001
-export const END_POINT_MAIL = state == "dev" ? `http://localhost:3000` : `https://outapi.outwittrader.com`
+export const END_POINT_MAIL = state === "dev" ? `http://localhost:3000` : `https://outapi.outwittrader.com`
